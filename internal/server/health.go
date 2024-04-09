b80a27b2c381549a4bfd7ccdd57cf46ef7074d95
Mason Schafer <mason.schafer1@gmail.com>
2024-04-08 21:15:44 -0400

    chore: set Cache-Control header for health checks so we don't cache the response down stream

--- internal/server/health.go
+++ internal/server/health.go
@@ -16,6 +16,8 @@ func (s *FiveMServer) GetHealthHandler(w http.ResponseWriter, r *http.Request) {
 	allocatedHeapMB := float64(mem.HeapAlloc) / 1024 / 1024
 	lastGC := time.Unix(0, int64(mem.LastGC)).String()
 
+	r.Header.Set("Cache-Control", "no-cache")
+
 	utils.RespondWithJSON(w, http.StatusOK, models.HealthResponse{
 		Status:    http.StatusText(http.StatusOK),
 		StartedAt: s.StartTime.String(),
